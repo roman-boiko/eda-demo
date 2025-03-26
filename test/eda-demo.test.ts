790072d3e0e061d9634b9fba18038fa07ac1a474
roman-boiko <roman.boiko@gmail.com>
2025-03-26 12:37:11 +0100

    Initial commit

---
+++ test/eda-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EdaDemo from '../lib/eda-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eda-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EdaDemo.EdaDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
